1ead2efb872dd129e943a81088e007c4a448e074
aellis <aellis@indeed.com>
2023-12-04 16:51:22 -0500

    Clean up day 1 part 2

--- src/day01/part2.ts
+++ src/day01/part2.ts
@@ -6,34 +6,36 @@ const input = parseInput({split: {delimiter: '\n', mapper: (l: string) => String
 
 let sum = 0;
 
-const map = new Map<String, number>();
-    map.set('1', 1);
-    map.set('one', 1);
-    map.set('eno', 1);
-    map.set('2', 2);
-    map.set('two', 2);
-    map.set('owt', 2);
-    map.set('3', 3);
-    map.set('three', 3);
-    map.set('eerht', 3);
-    map.set('4', 4);
-    map.set('four', 4);
-    map.set('ruof', 4);
-    map.set('5', 5);
-    map.set('five', 5);
-    map.set('evif', 5);
-    map.set('6', 6);
-    map.set('six', 6);
-    map.set('xis', 6);
-    map.set('7', 7);
-    map.set('seven', 7);
-    map.set('neves', 7);
-    map.set('8', 8);
-    map.set('eight', 8);
-    map.set('thgie', 8);
-    map.set('9', 9);
-    map.set('nine', 9);
-    map.set('enin', 9);
+const map = new Map<String, number>([
+    ['1', 1],
+    ['one', 1],
+    ['eno', 1],
+    ['2', 2],
+    ['two', 2],
+    ['owt', 2],
+    ['3', 3],
+    ['three', 3],
+    ['eerht', 3],
+    ['4', 4],
+    ['four', 4],
+    ['ruof', 4],
+    ['5', 5],
+    ['five', 5],
+    ['evif', 5],
+    ['6', 6],
+    ['six', 6],
+    ['xis', 6],
+    ['7', 7],
+    ['seven', 7],
+    ['neves', 7],
+    ['8', 8],
+    ['eight', 8],
+    ['thgie', 8],
+    ['9', 9],
+    ['nine', 9],
+    ['enin', 9],
+]);
+
 
 for (let line of input) {
     const first = line.match(/[0-9]|one|two|three|four|five|six|seven|eight|nine/);
@@ -42,12 +44,12 @@ for (let line of input) {
     let digit1 = 0;
     let digit2 = 0;
     if (first) {
-        let d = map.get(first[0]);
-        if (d !== undefined) digit1 = d;
+        // @ts-ignore
+        digit1 = map.get(first[0]);
     }
     if (second) {
-        let d = map.get(second[0]);
-        if (d !== undefined) digit2 = d;
+        // @ts-ignore
+        digit2 = map.get(second[0]);
     }
     
     const lineCalibration = (digit1 * 10) + digit2;
