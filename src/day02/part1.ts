a6a1c84927b6aec6c06e24e426f794f3ffa054f4
aellis <aellis@indeed.com>
2023-12-05 14:55:03 -0500

    Use benchmark util

--- src/day02/part1.ts
+++ src/day02/part1.ts
@@ -1,4 +1,4 @@
-import { parseInput } from '../util';
+import { benchmark, parseInput } from '../util';
 import { mappingFunction } from './common';
 
 const input = parseInput({split: {delimiter: '\n', mapper: mappingFunction }});
@@ -7,17 +7,19 @@ const input = parseInput({split: {delimiter: '\n', mapper: mappingFunction }});
 
 let sumOfIds = 0;
 
-for (let lineInfo of input) {
-    let possible = true;
-    for (let draw of lineInfo.draws) {
-        if (draw.red > 12 || draw.green > 13 || draw.blue > 14) {
-            possible = false;
-            break;
+benchmark(() => {
+    for (let lineInfo of input) {
+        let possible = true;
+        for (let draw of lineInfo.draws) {
+            if (draw.red > 12 || draw.green > 13 || draw.blue > 14) {
+                possible = false;
+                break;
+            }
+        }
+        if (possible) {
+            sumOfIds += lineInfo.id;
         }
     }
-    if (possible) {
-        sumOfIds += lineInfo.id;
-    }
-}
+})
 
 export default sumOfIds;
